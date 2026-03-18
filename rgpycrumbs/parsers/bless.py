322799436656ad2372fd46ca76d77150c579436f
Rohit Goswami <rgoswami@ieee.org>
2026-03-18 22:17:02 +0100

    fix: use datetime.timezone.utc for Python 3.10 compat

--- rgpycrumbs/parsers/bless.py
+++ rgpycrumbs/parsers/bless.py
@@ -19,5 +19,5 @@ BLESS_LOG = re.compile(
 # Usage: mktime('2024-10-28T18:58:24Z') - mktime('2024-10-28T18:58:21Z')
 def BLESS_TIME(x: str) -> datetime.datetime:  # noqa: N802
     return datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%SZ").replace(
-        tzinfo=datetime.UTC
+        tzinfo=datetime.timezone.utc
     )
